5514018ab51a0bd14628189fd450664086b13056
tyztongyuze <1367208449@qq.com>
2020-08-18 14:19:02 +0800

    冒泡排序

--- test/main.c
+++ test/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 
-/*
- * 冒泡排序
- */
+void swap(int *a, int *b);
+void bubble(int *a, int n );
+void output(int *a, int n);
+
 int main() {
     int num=0, i, j, a[10]={0};
     scanf("%d", &num);
@@ -11,17 +12,32 @@ int main() {
     {
         scanf("%d", a+i);
     }
-    for (i = 0; i < num; i++)
-        for (j = 0; j < num - i -1; j++)
+    bubble(a, num);
+    output(a, num);
+    return 0;
+}
+void bubble(int *a, int n )
+{
+    /*
+     * 冒泡排序
+     */
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n - i - 1; ++j)
             if (a[j] > a[j+1])
-            {
-                int temp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
-            }
-    for (i = 0; i < num; i++)
+                swap(a+j,a+j+1);
+        }
+    }
+}
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void output(int *a, int n)
+{
+    for (int i = 0; i < num; i++)
     {
         printf("%d ", a[i]);
     }
-    return 0;
 }
