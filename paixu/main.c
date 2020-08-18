3a1eec9f0a30ebf82c3d4fc8105d2dfac45be718
tyztongyuze <1367208449@qq.com>
2020-08-18 14:26:13 +0800

    排序算法

--- paixu/main.c
+++ paixu/main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+//#include <stdlib.h>
+
+void swap(int *a, int *b);
+void bubble(int *a, int n );
+void output(int *a, int n);
 
 int main() {
-    printf("Hello, World!\n");
+    int num=0, i, a[10]={0};
+    scanf("%d", &num);
+    for ( i = 0; i < num; ++i)
+    {
+        scanf("%d", a+i);
+    }
+    bubble(a, num);
+    output(a, num);
     return 0;
+}
+void bubble(int *a, int n )
+{
+    /*
+     * 冒泡排序
+     */
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n - i - 1; ++j)
+            if (a[j] > a[j+1])
+                swap(a+j,a+j+1);
+}
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void output(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
 }
