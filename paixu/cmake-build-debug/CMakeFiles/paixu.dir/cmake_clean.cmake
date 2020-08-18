4c61db1ef8a8277d278c4e6a14810fe4463d9908
tyztongyuze <1367208449@qq.com>
2020-08-18 14:23:51 +0800

    排序算法

---
+++ paixu/cmake-build-debug/CMakeFiles/paixu.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paixu.dir/main.c.obj"
+  "libpaixu.dll.a"
+  "paixu.exe"
+  "paixu.exe.manifest"
+  "paixu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/paixu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
