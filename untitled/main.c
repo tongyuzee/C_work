2dac8754d61f71f6e736192f8056d5b039d0a262
tyztongyuze <1367208449@qq.com>
2020-08-23 16:50:46 +0800

    前置++和后置++的区别

--- untitled/main.c
+++ untitled/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int i=0, j=0;
+    int i=0, n=5;
+    while (--n>0){
+        /* ++a表示取a的地址，增加它的内容，然后把值放在寄存器中 */
+        i++;
+        printf("%d %d\n", n, i);
+    }
+    i=0;n=5;
+    while (n-->0){
+        /* a++表示取a的地址，把它的值装入寄存器，然后增加内存中的a的值 */
+        i++;
+        printf("%d %d\n", n, i);
+    }
 
-    i++ ;
-    j++;
-    int w = (i++);
-    int y = ++i;
-    printf("%d %d %d %d", i, j, w, y);
-    w = (j++);
-    y = (++j);
-    printf("%d %d %d %d", i, j, w, y);
-    printf("Hello, World!\n");
-    return 0;
 }
 
