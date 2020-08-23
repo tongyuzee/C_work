36342226a1c535ae73afd85a1663d72aaf08ee6e
tyztongyuze <1367208449@qq.com>
2020-08-23 16:29:19 +0800

    clean project

--- untitled/main.c
+++ untitled/main.c
@@ -3,15 +3,15 @@
 int main() {
     int i=0, j=0;
 
-    i ++ ;
-    int w = i++;
+    i++ ;
+    j++;
+    int w = (i++);
     int y = ++i;
     printf("%d %d %d %d", i, j, w, y);
-    w = 0;
-    y = 0;
     w = (j++);
     y = (++j);
     printf("%d %d %d %d", i, j, w, y);
     printf("Hello, World!\n");
     return 0;
 }
+
