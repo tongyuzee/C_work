61c6b37f4d26420ca51b2ba0e5b695a075b215f0
tyztongyuze <1367208449@qq.com>
2020-08-30 13:23:16 +0800

    动态分配

--- List/main.c
+++ List/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+    int *A, i, j, m, n;
+    scanf("%d %d", &m, &n);
+    A = (int *)malloc(m * n *sizeof(int));
+    for(i=0;i<m;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",A+i*m+j);
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+            printf("%d ",*(A+i*m+j));
+        printf("\n");
+    }
+    free(A);
     printf("Hello, World!\n");
     return 0;
 }
