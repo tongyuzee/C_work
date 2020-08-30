b661c2defdea8b6811b194cdab69092e28bb2c6c
tyztongyuze <1367208449@qq.com>
2020-08-30 14:20:15 +0800

    创建并遍历列表

--- List/main.c
+++ List/main.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct tagNode{
+    int data;
+    struct tagNode *next;
+} LNode, *LinkList;
+void CreatLinkR(LinkList L, int n);
+void TraverseList(LinkList L);
 
 int main() {
-    int i, n;
-    char **p;
-    scanf("%d", &n);
-    p = (char **)malloc(n * sizeof(char *));
-    for (i = 0; i < n; i++) {
-        p[i] = malloc(100*sizeof(char));
-        gets(p[i]);
-    }
-    for(i=0;i<n;i++) puts(p[i]);
-    for(i=0;i<n;i++) free(p[i]);
-    free(p);
+    LinkList L;
+    L = (LinkList)malloc(sizeof(LNode));
+    int n;
+    scanf("%d",&n);
+    CreatLinkR(L, n);
+    TraverseList(L);
     printf("Hello, World!\n");
     return 0;
 }
 
+void CreatLinkR(LinkList L, int n)
+{
+    LinkList p, s;
+    p = L;
+    while (n-->0) {
+        s = (LinkList)malloc(sizeof(LNode));
+        scanf("%d",&s->data);
+        p->next = s;
+        p = s;
+    }
+    p->next = NULL;
+}
+void TraverseList(LinkList L)
+{
+    LinkList p = L->next;
+    while (p != NULL){
+        printf("%d ",p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
