9e44086002d9e6f4754d424b6a52d0ac1f19faa3
tyztongyuze <1367208449@qq.com>
2020-08-30 17:20:03 +0800

    创建并遍历链表

--- List/main.c
+++ List/main.c
@@ -10,21 +10,30 @@ void CreatLinkR(LinkList L, int n);
 void CreatLinkL(LinkList L, int n);
 void TraverseList(LinkList L);
 int LastElem(LinkList L);
+void LinkContact(LinkList L1, LinkList *L2);
 
 int main() {
 
-    LinkList L;
-    InitList(&L);
+    LinkList L1, L2;
+    InitList(&L1);
+    InitList(&L2);
 
-    int n;
-    scanf("%d",&n);
-    printf("1.头插法创建链表：\n");
-    CreatLinkL(L, n);
-    TraverseList(L);
-    printf("2.尾插法创建链表：\n");
-    CreatLinkR(L, n);
-    TraverseList(L);
-    printf("链表的最后一个元素是：%d", LastElem(L));
+    int n, m;
+    scanf("%d %d",&n,&m);
+
+    printf("头插法创建链表L1：\n");
+    CreatLinkL(L1, n);
+    TraverseList(L1);
+    printf("L1链表的最后一个元素是：%d\n", LastElem(L1));
+
+    printf("尾插法创建链表L2：\n");
+    CreatLinkR(L2, m);
+    TraverseList(L2);
+
+    printf("将链表L2接在L1的后面：\n");
+    LinkContact(L1, &L2);
+    TraverseList(L1);
+
     return 0;
 }
 
@@ -85,4 +94,21 @@ int LastElem(LinkList L)
         p = p->next;
     }
     return e;
+}
+void LinkContact(LinkList L1, LinkList *L2)
+{
+    /*
+     * 将链表L2接在L1的后面
+     */
+    LinkList p = L1->next, q = NULL;
+    while (p != NULL) {
+        q = p;
+        p = p->next;
+    }
+    if (q != NULL && *L2 != NULL) {
+        q->next = (*L2)->next;
+        free(*L2);
+        *L2 = NULL;
+    }
+
 }
