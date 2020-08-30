3ac30d389a6d035f1110cf273c9bf478191fa372
tyztongyuze <1367208449@qq.com>
2020-08-30 13:36:51 +0800

    动态分配字符串数组

--- List/main.c
+++ List/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main() {
-    int *A, i, j, m, n;
-    scanf("%d %d", &m, &n);
-    A = (int *)malloc(m * n *sizeof(int));
-    for(i=0;i<m;i++)
-        for(j=0;j<n;j++)
-            scanf("%d",A+i*m+j);
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<n;j++)
-            printf("%d ",*(A+i*m+j));
-        printf("\n");
+    int i, n;
+    char **p;
+    scanf("%d", &n);
+    p = (char **)malloc(n * sizeof(char *));
+    for (i = 0; i < n; i++) {
+        p[i] = malloc(100*sizeof(char));
+        gets(p[i]);
     }
-    free(A);
+    for(i=0;i<n;i++) puts(p[i]);
+    for(i=0;i<n;i++) free(p[i]);
+    free(p);
     printf("Hello, World!\n");
     return 0;
 }
+
